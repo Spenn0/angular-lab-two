03156dc4ee83901d801c9ee4b15d42f60b7aecf6
Spenn0 <sabcompositions@gmail.com>
2021-02-17 09:06:51 -0500

    here's my lab 2

---
+++ src/app/social-posts/social-posts.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../interfaces/post';
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css'],
+})
+export class SocialPostsComponent implements OnInit {
+  myPosts: Post[] = [
+    {
+      title: 'Something about Tuesday',
+      thought: 'Today seems long',
+    },
+    {
+      title: 'Paczki',
+      thought: 'Gimme that deliciousness',
+    },
+    {
+      title: 'Wowee',
+      thought: "I'm feeling pretty good today",
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDelete = (index: number): void => {
+    this.myPosts.splice(index, 1);
+  };
+
+  onSubmit = (post: Post): void => {
+    this.myPosts.unshift(post);
+  };
+}
